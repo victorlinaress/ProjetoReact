382b8cf57fbfe1b3f91ff142af5c8d6bc6a50e2b
unknown <victorbr8@outlook.com>
2025-02-15 18:04:58 -0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import test from '@/components/test';
+
+const Page = () => {
+  return (
+    <div>
+      <h1>Olá mundo</h1>
+    </div>
+  );
+};
+
+export default Page
---
+++ src/components/test.tsx
@@ -0,0 +1,10 @@
+const Test = () => {
+    return (
+      <div className="w-52 h-52">
+        SANTOS FUTEBOL CLUBE
+      </div>
+    );
+  };
+  
+  export default Test;
+  
