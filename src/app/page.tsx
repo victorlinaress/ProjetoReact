49f3040b32769bebc1c5305e7719341649af1acf
unknown <victorbr8@outlook.com>
2025-02-18 15:00:19 -0300

    teste

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,22 @@
-import test from "@/components/test";
-
 const Page = () => {
+  const fullTime = new Intl.DateTimeFormat("pt-BR", {
+    timeStyle: "short",
+    hour12: false,
+  }).format();
+
+  const hour = new Date().getHours();
+
   return (
-    <div>
-      <h1>Olá mundo</h1>
+    <div className="w-screen h-screen flex flex-col justify-center items-center text-white bg-gradient-to-r from-sky-500 to-indigo-500">
+      <div>
+        {hour >= 0 && hour <= 12 && "Bom dia"}
+        {hour >= 12 && hour <= 18 && "Boa tarde"}
+        {hour >= 18 && hour <= 23 && "Boa noite"}
+      </div>
+      <div className="text-9xl">{fullTime}</div>
+      <div className="text-5xl">Bom Teste</div>
     </div>
   );
 };
 
 export default Page;
-
-teste
